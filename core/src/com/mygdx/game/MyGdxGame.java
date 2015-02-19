fa740337ff6b46bf606ecaf95c833678038aa4b6
LuisMirandela <lmirandela@swplus.com.mx>
2015-02-19 15:37:46 -0600

    with music

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,27 +7,38 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.Audio;
-
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-    private Sound bgMusic;
+    Sound bgMusic;
+    //BUTTON
+    TextureAtlas atlas;
+    Skin skin;
+    Stage stage;
+    Table table;
 
 
     @Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("cover_0000s_0004_Cover.png");
+
+        bgMusic = Gdx.audio.newSound(Gdx.files.internal("groove.mp3"));
+        waitForLoadCompleted(bgMusic);
+        bgMusic.loop();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        bgMusic = Gdx.audio.newSound(Gdx.files.internal("army.mp3"));
 
         Sprite pantalla;
         pantalla = new Sprite(img);
@@ -37,20 +48,31 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		batch.begin();
 		//batch.draw(img, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight(), 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight(),false,false);
+
+        //BUTTON
+        stage = new Stage();
+        atlas = new TextureAtlas("btnConfigPack");
+        skin = new Skin(atlas);
+        table = new Table(skin);
+
+
         pantalla.draw(batch);
         batch.end();
-
-        //bgMusic.setLooping(true);
-        bgMusic.play();
-
-
-
 	}
-
 
     @Override
     public void dispose() {
         batch.dispose();
+        bgMusic.dispose();
+    }
+
+    private long waitForLoadCompleted(Sound sound) {
+        long id;
+        while ((id = sound.play(0)) == -1) {
+            long t = TimeUtils.nanoTime();
+            while (TimeUtils.nanoTime() - t < 100000000);
+        }
+        return id;
     }
 
 
