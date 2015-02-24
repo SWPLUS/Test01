16df1f88838e6462e4a4f8f06e57b5105b99f736
Ivan <ielizalde@swplus.com.mx>
2015-02-24 16:49:28 -0600

    WebService

---
+++ core/src/com/mygdx/game/LoginScreen.java
@@ -0,0 +1,118 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.Net.HttpRequest;
+import com.badlogic.gdx.Net.HttpMethods;
+import com.badlogic.gdx.Net.HttpResponse;
+import com.badlogic.gdx.Net.HttpResponseListener;
+
+import org.json.JSONObject;
+
+
+
+/**
+ * Created by Ivan on 24/02/15.
+ */
+
+
+public class LoginScreen implements Screen {
+    final MainScreen game;
+    private Stage stage;
+
+    Texture img;
+    Image imgBack;
+
+
+    public LoginScreen(final MainScreen gam) {
+        game = gam;
+
+        img = new Texture("Settings/background.png");
+        imgBack = new Image(img);
+        imgBack.setBounds(0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+        stage = new Stage();        //** window is stage **//
+        stage.clear();
+        Gdx.input.setInputProcessor(stage); //** stage is responsive **//
+
+        stage.addActor(imgBack);
+
+
+
+
+
+        HttpRequest httpPost = new HttpRequest(HttpMethods.POST);
+        httpPost.setUrl("http://tang.com.mx/webservices/ws003");
+        httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        httpPost.setContent("username=swplus&password=tokey");
+
+
+        Gdx.net.sendHttpRequest (httpPost, new HttpResponseListener() {
+            public void handleHttpResponse(HttpResponse httpResponse) {
+
+                JSONObject jObject = new JSONObject(httpResponse.getResultAsString());
+                JSONObject success = jObject.getJSONObject("success");
+
+                Gdx.app.log("my app", success.toString());
+            }
+            public void failed(Throwable t) {
+                Gdx.app.log("my app", t.getMessage());
+            }
+
+            public void cancel() {
+            }
+
+            public void cancelled() {
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+
+        game.batch.begin();
+
+        stage.draw();
+        game.batch.end();
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    public void dispose() {
+    }
+
+}
