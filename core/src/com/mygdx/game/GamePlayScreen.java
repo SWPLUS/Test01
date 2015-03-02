fc9c68ee4f8a05d290678139c28daeb2d41027b1
Ivan <ielizalde@swplus.com.mx>
2015-03-02 17:02:19 -0600

    Seleccion de Nivel

--- core/src/com/mygdx/game/GamePlayScreen.java
+++ core/src/com/mygdx/game/GamePlayScreen.java
@@ -4,8 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 /**
  * Created by Ivan on 02/03/15.
@@ -19,22 +22,50 @@ public class GamePlayScreen implements Screen {
     Texture img;
     Image imgBack;
 
-    String Header;
+    private BitmapFont font;
+    private TextureAtlas AtlasHeader;
+    private Skin SkinHeader;
+
+    private float ScreenWidth, ScreenHeight;
+
+    Image HeaderImage;
+    String HeaderName;
+    int Lives = 5;
+    int Special = 0;
+
 
 
     public GamePlayScreen(final MainScreen gam, int lev) {
         game = gam;
         Level = lev;
 
+
+
         img = new Texture("Settings/background.png");
         imgBack = new Image(img);
         imgBack.setBounds(0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
-        stage = new Stage();        //** window is stage **//
+        AtlasHeader = new TextureAtlas("GamePlay/colors.txt");
+        SkinHeader = new Skin();
+        SkinHeader.addRegions(AtlasHeader);
+        font = new BitmapFont(Gdx.files.internal("fonts/white.fnt"),false);
+
+        ScreenWidth = Gdx.graphics.getWidth();
+        ScreenHeight = Gdx.graphics.getHeight();
+
+        stage = new Stage();
         stage.clear();
-        Gdx.input.setInputProcessor(stage); //** stage is responsive **//
+        Gdx.input.setInputProcessor(stage);
+
+        HeaderName = "levels-color-000" + Level;
+        HeaderImage = new Image();
+        HeaderImage.setDrawable(SkinHeader.getDrawable(HeaderName));
+        HeaderImage.setBounds(0,ScreenHeight - game.calcSize(138,false),
+                game.calcSize(1080,true),game.calcSize(138,false));
+
 
         stage.addActor(imgBack);
+        stage.addActor(HeaderImage);
 
 
     }
