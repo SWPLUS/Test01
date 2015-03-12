ff47b364ee37c5264a4caa6c89bc72c6c6917aec
LuisMirandela <lmirandela@swplus.com.mx>
2015-03-12 15:25:17 -0600

    removed warnings

--- core/src/com/mygdx/game/GamePlayScreen.java
+++ core/src/com/mygdx/game/GamePlayScreen.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 //import com.badlogic.gdx.scenes.scene2d.InputEvent;
 //import com.badlogic.gdx.scenes.scene2d.InputListener;
 //import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
